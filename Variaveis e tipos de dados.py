9f08147671941457f61135224ae55d2c7a4e1d8c
davisdenercarneiro <davisdener@hotmail.com>
2025-04-24 11:17:15 -0300

    Variaveis e tipos de dados

---
+++ Variaveis e tipos de dados.py
@@ -0,0 +1,14 @@
+Declarar variavel:
+nome_da_variavel = "valor"
+
+Adicionar variavel:
+
+print (f"texto texto {nome_da_variavel} texto texto")
+
+Tipos de dados:
+    string : "texto"
+    int : 10
+    float : 10.5
+    bool true / false 
+
+    
